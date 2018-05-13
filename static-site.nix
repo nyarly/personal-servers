e1c0d88dab2ab9f286a962df3f4b350a6defd6b0
Judson <nyarly@gmail.com>
2018-05-13 16:42:31 -0700

    Working multiple site module, plus one-step ACME

---
+++ static-site.nix
@@ -0,0 +1,91 @@
+{ config, lib, pkgs, ...}:
+with lib;
+{
+  options = {
+    staticWeb = {
+        acmeRoot = mkOption {
+        type = with types; path;
+        description = "The path that ACME clients will use for challenges.";
+      };
+
+      sites = mkOption {
+        default = {};
+        type = with types; attrsOf (submodule {
+          options = {
+            docRoot = mkOption { type = path; };
+          };
+        });
+        description = ''
+          Attribute set of static sites.
+        '';
+
+        example = literalExample ''
+          judsonlester.info = {
+            docRoot = blog;
+          };
+        '';
+      };
+    };
+  };
+
+  config = {
+    services.httpd.virtualHosts = let
+      vhosts =  concatLists (mapAttrsToList nameToVHost config.staticWeb.sites);
+      nameToVHost = name: hcfg:
+      let
+        inherit (hcfg) docRoot;
+      in
+      [
+        {
+          hostName = name;
+          serverAliases = [ "www.${name}" ];
+          listen = [{ port = 80; }];
+
+          documentRoot = docRoot;
+          extraConfig = ''
+                Redirect / https://${name}
+                Alias "/.well-known/acme-challenge" "${config.staticWeb.acmeRoot}/${name}/.well-known/acme-challenge"
+                <Directory ${config.staticWeb.acmeRoot}/${name}>
+                  Require all granted
+                </Directory>
+          '';
+        }
+
+        {
+          hostName = name;
+          serverAliases = [ "www.${name}" ];
+          listen = [{ port = 443; }];
+
+          documentRoot = docRoot;
+          enableSSL = true;
+          sslServerCert = "/var/lib/acme/${name}/full.pem";
+          sslServerKey = "/var/lib/acme/${name}/key.pem";
+
+          extraConfig = ''
+                Alias "/.well-known/acme-challenge" "${config.staticWeb.acmeRoot}/${name}/.well-known/acme-challenge"
+                <Directory ${config.staticWeb.acmeRoot}/${name}>
+                  Require all granted
+                </Directory>
+          '';
+        }
+      ];
+    in vhosts;
+
+    security.acme.certs = let
+      certs = listToAttrs (concatLists (mapAttrsToList siteToCertCfg config.staticWeb.sites));
+      siteToCertCfg = domain: {...}:
+      let
+        cfg = {
+            webroot = config.staticWeb.acmeRoot + "/${domain}";
+            email = "nyarly@gmail.com";
+            postRun = "systemctl reload httpd.service";
+          };
+      in [ { name = domain; value = cfg; } { name = "www.${domain}"; value = cfg; } ];
+    in certs;
+
+    systemd.services.httpd = {
+      after = [ "acme-selfsigned-certificates.target" ];
+      wants = [ "acme-selfsigned-certificates.target" "acme-certificates.target" ];
+    };
+  };
+}
--- webserver.nix
+++ webserver.nix
@@ -1,76 +1,31 @@
 let
   blog = import ./blog/default.nix {};
   acmeRoot = "/var/run/acme-challenges";
-
-
-  httpDomainConfig = {name, docRoot, tlsReady ? false}: {
-          enable = true;
-          adminAddr = "nyarly@gmail.com";
-          virtualHosts = [
-            {
-              hostName = name;
-              serverAliases = [ "www.${name}" ];
-              listen = [{ port = 80; }];
-
-              documentRoot = docRoot;
-              extraConfig = ''
-                ${if tlsReady then "Redirect / https://${name}" else ""}
-                Alias "/.well-known/acme-challenge" "${acmeRoot}/${name}/.well-known/acme-challenge"
-                <Directory ${acmeRoot}/${name}>
-                  Require all granted
-                </Directory>
-              '';
-
-            }
-
-            {
-              hostName = name;
-              serverAliases = [ "www.${name}" ];
-              listen = [{ port = 443; }];
-
-              documentRoot = docRoot;
-              enableSSL = true;
-              sslServerCert = "/var/lib/acme/${name}/full.pem";
-              sslServerKey = "/var/lib/acme/${name}/key.pem";
-
-              extraConfig = ''
-                Alias "/.well-known/acme-challenge" "${acmeRoot}/${name}/.well-known/acme-challenge"
-                <Directory ${acmeRoot}/${name}>
-                  Require all granted
-                </Directory>
-              '';
-            }
-          ];
-        };
-
 in
   {
     network.description = "Web server";
 
-    webserver =
-      {  pkgs, ... }:
-      {
-        environment.systemPackages = with pkgs; [
-          neovim
-          fish
-        ];
+    webserver = {  pkgs, ... }:
+    {
+      imports = [ ./static-site.nix ];
 
-        services.fail2ban.enable = true;
+      environment.systemPackages = with pkgs; [ neovim fish ];
 
-        services.httpd = httpDomainConfig {
-          name = "judsonlester.info";
-          docRoot = blog;
-          tlsReady = true;
-        };
+      services.fail2ban.enable = true;
 
-        security.acme.certs = {
-          "judsonlester.info" = {
-            webroot = acmeRoot + "/judsonlester.info";
-            email = "nyarly@gmail.com";
-            postRun = "systemctl reload httpd.service";
-          };
-        };
-
-        networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+      services.httpd = {
+        enable = true;
+        adminAddr = "nyarly@gmail.com";
       };
+
+      staticWeb = {
+        inherit acmeRoot;
+        sites = {
+          "judsonlester.info" = { docRoot = blog; };
+          "madhelm.net"       = { docRoot = blog; };
+        };
+      };
+
+      networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+    };
   }
