d874be5d5d62ff7af25654fd07b67fd32fc80b12
Judson Lester <nyarly@gmail.com>
2025-02-23 16:26:35 -0800

    feat: sops secrets

--- flake.nix
+++ flake.nix
@@ -7,8 +7,13 @@
       url = "github:numtide/flake-utils";
     };
     deploy-rs.url = "github:serokell/deploy-rs";
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, deploy-rs, flake-utils }@inputs:
+  outputs = { self, nixpkgs, deploy-rs, flake-utils, sops-nix }@inputs:
     let
       mkDeployPkgs = system:
         let
@@ -28,26 +33,35 @@
 
       pkgs = nixpkgs.legacyPackages.${system};
 
-      deployPkgs = mkDeployPkgs system;
+        deployPkgs = mkDeployPkgs system;
 
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nixVersions.nix_2_17
-          # nixops # RIP
-          # Good a place as any to sketch out new approach:
-          # attributes here for server(s), which we can use nixos-rebuild --target to deploy
-          # * an association between targets and flake attributes makes sense...
-          #
-          # For provisioning: Terraform, or https://github.com/tweag/tf-ncl
-          # tf-ncl would make the config look a lot like(?) existing config
-          # ... then we'd need to import state etc.
-          git-crypt
-          nix-prefetch-git
-          deployPkgs.deploy-rs.deploy-rs
-        ];
-      };
-    })) // (let
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixVersions.nix_2_17
+            # nixops # RIP
+            # Good a place as any to sketch out new approach:
+            # attributes here for server(s), which we can use nixos-rebuild --target to deploy
+            # * an association between targets and flake attributes makes sense...
+            #
+            # For provisioning: Terraform, or https://github.com/tweag/tf-ncl
+            # tf-ncl would make the config look a lot like(?) existing config
+            # ... then we'd need to import state etc.
+            git-crypt
+            nix-prefetch-git
+            deployPkgs.deploy-rs.deploy-rs
+
+            sops
+            ssh-to-pgp
+            ssh-to-age
+            age
+          ];
+
+          nativeBuildInputs = [
+            sops-nix.packages.${system}.sops-import-keys-hook
+          ];
+        };
+      })) // (let
       system = flake-utils.lib.system.x86_64-linux;
 
       deployPkgs = mkDeployPkgs system;
@@ -59,7 +73,10 @@
 
       systemConfig = name: nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [ (./. + "/nodes/${name}/configuration.nix") ];
+        modules = [
+          sops-nix.nixosModules.sops
+          (./. + "/nodes/${name}/configuration.nix")
+        ];
         specialArgs = {
           inherit inputs;
           localPkgs = self.packages;
